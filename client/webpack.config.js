65895273092ef0ed809a9db8d836752bca061c88
brettfritz <brett.fritz3@gmail.com>
2024-08-06 20:04:32 -0500

    deleted/moved files, build bugs persist

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
       display: 'standalone',
       icons: [
         {
-          src: path.resolve('client/src/images/logo.png'),
+          src: path.resolve('src/images/logo.png'),
           sizes: [96, 128, 192, 256, 384, 512],
           destination: path.join('assets', 'icons'),
         },
